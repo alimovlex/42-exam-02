5f34981deb2d058ff8b38b0e4c72fff8d3228eab
robot <robot@recompile.me>
2026-04-17 15:58:47 +0200

    Added word match with explanations.

--- exam/wdmatch.c
+++ exam/wdmatch.c
@@ -1,5 +1,36 @@
 #include <unistd.h>
 
+void ft_wdmatch(char *s1, char *s2)
+{
+    // 1. We create a 'tracker' pointer (ptr).
+    // We need to keep the original 's1' pointing at the start so we can print it later.
+    char *ptr = s1;
+
+    // 2. We scan the second string (s2) entirely.
+    // We never stop scanning s2 until it hits '\0'.
+    while (*s2)
+    {
+        // 3. If the current character in s2 is the one we are looking for in s1:
+        if (*ptr == *s2)
+            ptr++; // Move the tracker to the next character we need to find.
+        // 4. Success Check:
+        // If our tracker hits '\0', it means we found every single char of s1 in s2.
+        if (*ptr == '\0')
+        {
+            // Now we print the original s1 from the start.
+            while (*s1)
+            {
+                write(1, s1, 1);
+                s1++;
+            }
+            return; // We are done. Exit the function.
+        }
+        // 5. Always move s2 forward to keep searching.
+        s2++;
+    }
+}
+
+/*
 void ft_wdmatch(char** argv)
 {
     char *a;
@@ -22,3 +53,4 @@ void ft_wdmatch(char** argv)
         write(1, s, a - s);
     write(1, "\n", 1);
 }
+*/
