c8259255dbd06de274c3c08dd52c177e12bced1a
robot <robot@recompile.me>
2026-04-22 00:06:45 +0200

    Simplified solution.

--- exam/ft_strrev.c
+++ exam/ft_strrev.c
@@ -2,17 +2,20 @@
 
 char* ft_strrev(char* str)
 {
-    char *start = str;
-    char *end = str;
+    char* start = str;
+    char* end = str;
+
     while(*end)
         end++;
     end--;
 
-    while(start < end)
+    while(end > start)
     {
         char tmp = *start;
-        *start++ = *end;
-        *end-- = tmp;
+        *start = *end;
+        *end = tmp;
+        start++;
+        end--;
     }
     return str;
 }
