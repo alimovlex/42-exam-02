6d3e600e681debb2dc417274f6911d66d57c75e5
alalimov <alalimov@student.codam.nl>
2026-06-01 13:57:49 +0200

    Optimized solution.

--- exam/fizzbuzz.c
+++ exam/fizzbuzz.c
@@ -14,34 +14,32 @@
 
 void ft_fizzbuzz()
 {
-    int counter = 1, decimal = 0, digit = 0;
     char *fizz = "fizz\n", *buzz = "buzz\n", *fizzbuzz = "fizzbuzz\n";
-    char* fizz_start = fizz;
-    char* buzz_start = buzz;
-    char* fizzbuzz_start = fizzbuzz;
-    while(counter <= 100)
+    char *fizz_ptr = fizz, *buzz_ptr = buzz, *fizzbuzz_ptr = fizzbuzz;
+    int i = 1, decimal = 0, digit = 0;
+    while(i <= 100)
     {
-        fizz = fizz_start;
-        buzz = buzz_start;
-        fizzbuzz = fizzbuzz_start;
-        decimal = counter / 10 + '0';
-        digit = counter % 10 + '0';
-        if (counter % 3 == 0 && counter % 5 == 0)
-            while(*fizzbuzz)
-                write(1, fizzbuzz++, 1);
-        else if (counter % 3 == 0)
-            while(*fizz)
-                write(1, fizz++, 1);
-        else if (counter % 5 == 0)
-            while(*buzz)
-                write(1, buzz++, 1);
+        fizz_ptr = fizz;
+        buzz_ptr = buzz;
+        fizzbuzz_ptr = fizzbuzz;
+        decimal = i / 10 + '0';
+        digit = i % 10 + '0';
+        if ((i % 3 == 0) && (i % 5 == 0))
+            while(*fizzbuzz_ptr)
+                write(1, fizzbuzz_ptr++, 1);
+        else if (i % 3 == 0)
+            while(*fizz_ptr)
+                write(1, fizz_ptr++, 1);
+        else if (i % 5 == 0)
+            while(*buzz_ptr)
+                write(1, buzz_ptr++, 1);
         else
         {
-            if (counter >= 10)
+            if (i >= 10)
                 write(1, &decimal, 1);
             write(1, &digit, 1);
             write(1, "\n", 1);
         }
-        counter++;
+        i++;
     }
 }
