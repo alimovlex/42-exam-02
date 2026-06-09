2c1826bb4e80603bf692ee1265c7d6b97882deac
alalimov <alalimov@student.codam.nl>
2026-06-09 15:59:49 +0200

    Refined solution based on snake_to_camel solution.

--- exam/camel_to_snake.c
+++ exam/camel_to_snake.c
@@ -14,19 +14,16 @@
 
 void camel_to_snake(char *str)
 {
-    while (*str)
+    while(*str)
     {
-        // Check if the current character is an uppercase letter
         if (*str >= 'A' && *str <= 'Z')
         {
             // Print the underscore separator before the word
-            write(1, "_", 1);
-            // Convert to lowercase by adding 32 (ASCII offset)
+            //write(1, "_", 1);
             *str += 32;
+            *--str = '_';
         }
-        // Print the character (either original lowercase or converted)
-        write(1, str, 1);
-        str++;
+        write(1, str++, 1);
     }
     write(1, "\n", 1);
 }
