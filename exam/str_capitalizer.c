722138ebe134b06260b66dcbb4eb8dcb74e9d2dd
alalimov <alalimov@student.codam.nl>
2026-06-17 12:29:39 +0200

    Wrote more compact solution!

--- exam/str_capitalizer.c
+++ exam/str_capitalizer.c
@@ -14,6 +14,25 @@
 
 int ft_is_space(char c);
 
+void ft_strcapitalizer(char *str)
+{
+    if (*str >= 'a' && *str <= 'z')
+        *str -= 32;
+    write(1, str++, 1);
+    while(*str)
+    {
+        if ((*str | 32) >= 'a' && (*str | 32) <= 'z')
+        {
+            if (*str >= 'A' && *str <= 'Z')
+                *str += 32;
+            if (ft_is_space(*(str - 1)))
+                *str -= 32;
+        }
+        write(1, str++, 1);
+    }
+    write(1, "\n", 1);
+}
+/*
 void str_capitalizer(char *str)
 {
     // Protect against empty strings to prevent a segfault
@@ -45,7 +64,6 @@ void str_capitalizer(char *str)
     write(1, "\n", 1);
 }
 
-/*
 void str_capitalizer(char *str)
 {
     // 1. Handle the first character explicitly so we can safely look back later
