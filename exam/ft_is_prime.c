ca3e54ca017e5ef8779ef7a4631db4bbe6e47e1b
robot <robot@recompile.me>
2026-06-17 20:23:28 +0200

    Added other helper function to solve the exercise.

---
+++ exam/ft_is_prime.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+int is_prime(int a)
+{
+    //Prime numbers starts from 2
+    int i = 2;
+
+    if (a <= 1)
+        return (0);
+    while (i * i <= a)
+    {
+        if (a % i == 0)
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
