12c733ec93d437a649898d6ee4f5f872e124a814
alalimov <alalimov@student.codam.nl>
2026-06-20 18:19:02 +0200

    Found perfect fprime solution that suits me.

--- exam/fprime.c
+++ exam/fprime.c
@@ -13,8 +13,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int ft_is_prime(int a);
+//int ft_is_prime(int a);
 
+void fprime(char *str)
+{
+    int num = atoi(str);
+    int result = 1;
+    int i = 2;
+
+    if (num == 1)
+    {
+        printf("1\n");
+        return;
+    }
+
+    while (result < num)
+    {
+        /* * OPTIMIZATION 1: (num / result) % i == 0
+         * We divide num by our current result first, THEN use modulo.
+         * This completely removes the risk of integer overflow.
+         */
+        if ((num / result) % i == 0)
+        {
+            printf("%d", i);
+            result *= i;          /* Build up our target number */
+
+            if (result < num)     /* Only print '*' if we aren't done building */
+                printf("*");
+        }
+        else
+            i++; /* Exhausted this factor, move to the next */
+    }
+    printf("\n");
+}
+
+/*
 void fprime(char *str)
 {
     int result = 1, i = 2, is_printed = 1;
@@ -42,7 +75,33 @@ void fprime(char *str)
     printf("\n");
 }
 
-/*
+
+void fprime(char *str)
+{
+    int n = atoi(str);
+    int i = 2;
+
+    if (n == 1)
+    {
+        printf("1");
+        return;
+    }
+    while (i <= n)
+    {
+        //ft_is_prime(i) is a reduntant check, just to understand the exercise
+        if (ft_is_prime(i) && (n % i == 0))
+        {
+            printf("%d", i);
+            n /= i;
+            if (n > 1)
+                printf("*");
+        }
+        else
+            i++;
+    }
+    printf("\n");
+}
+
 void fprime(char *str)
 {
   int n = atoi(str);
